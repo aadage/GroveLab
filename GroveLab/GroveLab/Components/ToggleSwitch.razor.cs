ed770a0b3e47704fa129098fd24fff7cfbbdd3ce
aadage <adage42@protonmail.com>
2023-11-11 14:56:20 -0500

    Created ToggleSwitch

---
+++ GroveLab/GroveLab/Components/ToggleSwitch.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GroveLab.Components;
+
+public partial class ToggleSwitch : TogglerBase
+{
+    private async Task CheckboxChanged(ChangeEventArgs e)
+    {
+        await Toggle();
+    }
+}
