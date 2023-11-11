c3bed0ca6c5987fec2ad22f6ce34da762d258294
aadage <adage42@protonmail.com>
2023-11-11 11:46:59 -0500

    created basic toggler

---
+++ GroveLab/GroveLab/Components/Toggler.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GroveLab.Components;
+
+public partial class Toggler
+{
+    [Parameter] public bool Enabled { get; set; } = false;
+    [Parameter] public EventCallback<bool> EnabledChanged { get; set; }
+
+    private async Task Toggle()
+    {
+        Enabled = !Enabled;
+        await EnabledChanged.InvokeAsync(Enabled);
+    }
+}
